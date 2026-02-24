b1e1dad182b3145c5116ca57c43d4551e4bb45c8
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-02-24 20:35:45 +0600

    Deleted a file

--- LeetCode/Arrays/125-valid-palindrome/valid-palindrome.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s1 = str()
-        for u in s:
-            if('a' <= u <= 'z' or 'A' <= u <= 'Z' or '0' <= u <= '9'):
-                s1 += u.lower() if('A' <= u <= 'Z') else u
-        return s1 == s1[::-1]
