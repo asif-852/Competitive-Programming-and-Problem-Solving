fdb85b4b28aeb3a94d951212d18cdaf901d8553d
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-02-25 06:09:57 +0600

    Time: 59 ms (23.03%) | Memory: 92.8 MB (16.80%) - LeetSync

--- LeetCode/Arrays/217-contains-duplicate/contains-duplicate.cpp
+++ LeetCode/Arrays/217-contains-duplicate/contains-duplicate.cpp
@@ -1,7 +1,25 @@
+// Approach 1: Time Complexity: O(N), Space Complexity: O(N)
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> s(nums.begin(), nums.end());
-        return (s.size() != nums.size());
+        unordered_set<int> s(nums.begin(), nums.end());
+        return s.size() != nums.size();
     }
 };
+
+
+// Approach 2: Time Complexity: O(NlogN), Space Complexity: O(1)
+/*
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;        
+    }
+};
+*/
