e4118b2f94d3e6b6b7d8c6b6dc3a031eb4191b52
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-02-28 21:19:50 +0600

    Time: 0 ms (100.00%) | Memory: 18.7 MB (52.06%) - LeetSync

---
+++ LeetCode/Arrays/73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,75 @@
+// Approach 1: Space Complexity: O(1)
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        bool firstRowBad = 0, firstColBad = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    if(i == 0) {
+                        firstRowBad = 1;
+                    }
+                    if(j == 0) {
+                        firstColBad = 1;
+                    }
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int j = 0; j < n; j++) {
+            if(firstRowBad) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            if(firstColBad) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
+// Approach 2: Space Complexity: O(m + n)
+
+/*
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> badrows, badcolumns;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    badrows.push_back(i);
+                    badcolumns.push_back(j);
+                }
+            }
+        }
+        for(int i : badrows) {
+            for(int j = 0; j < n; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+        for(int j : badcolumns) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};
+
+*/
