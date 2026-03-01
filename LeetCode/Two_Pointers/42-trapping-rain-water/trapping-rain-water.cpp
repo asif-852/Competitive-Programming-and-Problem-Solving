82a1f62cde088a3c836d03473c75e49c85eafc67
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-01 13:50:10 +0600

    Time: 6 ms (5.53%) | Memory: 27.2 MB (27.89%) - LeetSync

---
+++ LeetCode/Two_Pointers/42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        vector<int> pref_max = height, suff_max = height;
+        partial_sum(pref_max.begin(), pref_max.end(), pref_max.begin(), [](int a, int b) {
+            return max(a, b);
+        });
+        partial_sum(suff_max.rbegin(), suff_max.rend(), suff_max.rbegin(), [](int a, int b) {
+            return max(a, b);
+        });
+        long long ans = 0;
+        for(int i = 0; i < height.size(); i++) {
+            ans += min(pref_max[i], suff_max[i]) - height[i];
+        }
+        return ans;
+    }
+};
