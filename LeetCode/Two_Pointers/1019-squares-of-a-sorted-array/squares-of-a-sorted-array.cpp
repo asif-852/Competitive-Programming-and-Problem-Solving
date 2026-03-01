eb87f02d39789db819c885487a1447b7c2d38278
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-01 14:02:48 +0600

    Time: 0 ms (100.00%) | Memory: 31.3 MB (10.57%) - LeetSync

---
+++ LeetCode/Two_Pointers/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(int l = 0, r = n - 1; l <= r;) {
+            if(abs(nums[l]) >= abs(nums[r])) {
+                ans.push_back(nums[l] * nums[l]);
+                l++;
+            } else {
+                ans.push_back(nums[r] * nums[r]);
+                r--;
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
