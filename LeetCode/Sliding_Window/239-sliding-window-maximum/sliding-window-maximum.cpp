0c88120ceb20212db0ea428159a68df340c6a0bf
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-02 20:34:44 +0600

    Time: 23 ms (59.64%) | Memory: 139.2 MB (67.32%) - LeetSync

--- LeetCode/Sliding_Window/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ LeetCode/Sliding_Window/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,3 +1,32 @@
+// Approach 1: Time Complexity: O(n), Space Complexity: O(k)
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+        for(int i = 0; i < nums.size(); i++) {
+            // Ensuring outdated elements are out of the window
+            while(!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+            // Ensuring decreasing order
+            while(!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
+
+// Approach 2: Time Complexity: O(nlogk), Space Complexity: O(k)
+
+/*
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -15,3 +44,5 @@ public:
         return ans;
     }
 };
+
+*/
