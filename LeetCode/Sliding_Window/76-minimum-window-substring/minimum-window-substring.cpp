be5c842bd0426627f69a415ff31131f5c6a3d102
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-02 19:56:13 +0600

    Time: 4 ms (70.87%) | Memory: 11.6 MB (42.95%) - LeetSync

--- LeetCode/Sliding_Window/76-minimum-window-substring/minimum-window-substring.cpp
+++ LeetCode/Sliding_Window/76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    bool enough(unordered_map<char, int>& cnt1, unordered_map<char, int>& cnt2) {
-        bool ok = 1;
-        for(auto [c, freq] : cnt2) {
-            ok &= (cnt1[c] >= freq);
-        }
-        return ok;
-    }
-
     string minWindow(string s, string t) {
         int m = s.size();
-        unordered_map<char, int> cnt1, cnt2;
+        // unordered_map<char, int> cnt1, cnt2;
+        // vector<int> cnt1(52), cnt2(52); // also works but want to avoid cnt[c - 'a'] kind of thing here for better readability.
+        vector<int> cnt1(128), cnt2(128);
+        int formed = 0, required = 0;
         for(auto u : t) {
+            if(cnt2[u] == 0) {
+                required++;
+            }
             cnt2[u]++;
         }
         int len = 1e9, l = 0, ans_l = -1;
         for(int r = 0; r < m; r++) {
-            cnt1[s[r]]++;
-            while(enough(cnt1, cnt2)) {
+            char c = s[r];
+            cnt1[c]++;
+            if(cnt2[c] && cnt1[c] == cnt2[c]) {
+                formed++;
+            }
+            while(formed == required) {
                 if((r - l + 1) <= len) {
                     len = r - l + 1;
                     ans_l = l;
                 }
-                cnt1[s[l]]--;
+                char left_c = s[l];
+                if(cnt2[left_c] && cnt1[left_c] == cnt2[left_c]) {
+                    formed--;
+                }
+                cnt1[left_c]--;
                 l++;
             } 
             
