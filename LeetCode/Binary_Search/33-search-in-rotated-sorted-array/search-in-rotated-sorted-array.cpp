eccb3c83049255b4dad89efc3d8c652a78745bff
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-03 21:27:38 +0600

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.54%) - LeetSync

---
+++ LeetCode/Binary_Search/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int ans = -1;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(nums[mid] == target) {
+                ans = mid;
+                break;
+            }
+            if(nums[l] <= nums[mid]) {
+                // Left portion is evidently sorted
+                if(target <= nums[mid] && target >= nums[l]) {
+                    // Target is in the left portion
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                // Right portion is evidently sorted
+                if(target >= nums[mid] && target <= nums[r]) {
+                    // Target is in the right portion
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
