b04fdb077f64dfbeaae38ab40a83a8cfdd19ca12
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-05 18:59:05 +0600

    Time: 61 ms (47.85%) | Memory: 72.7 MB (51.77%) - LeetSync

---
+++ LeetCode/Priority_Queue/1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    struct Point{
+        int x, y;
+    };
+    static int distance(Point p) {
+        return p.x * p.x + p.y * p.y;
+    } 
+    struct Compare {
+        bool operator()(const Point& l, const Point& r) const {
+            return distance(l) < distance(r);
+        }
+    };
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<Point, vector<Point>, Compare> pq;
+        for(auto u : points) {
+            Point p = {u[0], u[1]};
+            pq.push(p);
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<vector<int>> ans;
+        while(!pq.empty()) {
+            Point p = pq.top();
+            pq.pop();
+            ans.push_back({p.x, p.y});
+        }
+        return ans;
+    }
+};
