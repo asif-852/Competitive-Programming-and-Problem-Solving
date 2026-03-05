b139d653899c4b9898c7787db1aaa209efd7d635
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-05 19:47:02 +0600

    Time: 2 ms (56.51%) | Memory: 35.7 MB (99.35%) - LeetSync

--- LeetCode/Priority_Queue/658-find-k-closest-elements/find-k-closest-elements.cpp
+++ LeetCode/Priority_Queue/658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,3 +1,23 @@
+// Approach 1: Binary Search + Two pointers, Time Complexity: O(log(n - k) + k), Space Complexity: O(1)
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size(); 
+        int r = lower_bound(arr.begin(), arr.end() - k, x) - arr.begin(); 
+        int l = r - 1;
+        while(k--) {
+            (l >= 0 && x - arr[l] <= arr[r] - x) ? l-- : r++;
+        }
+        return vector<int>(arr.begin() + l + 1, arr.begin() + r);
+    }
+};
+
+
+// Approach 2: Priority Queue, Time Complexity: O(n + klogk), Space Complexity: O(k)
+
+/*
+
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
@@ -18,3 +38,5 @@ public:
         return ans;
     }
 };
+
+*/
