d2aeabe52633cfc5261b669706f1867a70932558
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-08 14:22:32 +0600

    Time: 1821 ms (5.03%) | Memory: 381.5 MB (5.40%) - LeetSync

---
+++ LeetCode/BackTracking/79-word-search/word-search.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board, int i, int j, const string& word, int curr_depth) {
+        if(curr_depth == word.size()) {
+            return true;
+        }
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[curr_depth]) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+        
+        bool found = false;
+        const vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for(auto& [di, dj] : dirs) {
+            if(dfs(board, i + di, j + dj, word, curr_depth + 1)) {
+                found = true;
+                break;
+            }
+        }
+
+        board[i][j] = temp;
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == word[0]) {
+                    if(dfs(board, i, j, word, 0)) {
+                        return true; 
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
