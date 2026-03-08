3c160d5b4d0a7a14b6a004d5a31d2158989b43aa
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-03-08 13:30:12 +0600

    Time: 1 ms (85.70%) | Memory: 13 MB (82.51%) - LeetSync

--- LeetCode/BackTracking/22-generate-parentheses/generate-parentheses.cpp
+++ LeetCode/BackTracking/22-generate-parentheses/generate-parentheses.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+    void backtrack(int n, int open_count, int close_count, string& current_string, vector<string>& result) {
+        if(current_string.length() == 2 * n) {
+            result.push_back(current_string);
+            return;
+        }
+
+        if(open_count < n) {
+            current_string.push_back('(');                                      
+            backtrack(n, open_count + 1, close_count, current_string, result);  
+            current_string.pop_back();                                          
+        }
+        if(close_count < open_count) {
+            current_string.push_back(')');
+            backtrack(n, open_count, close_count + 1, current_string, result);
+            current_string.pop_back();                                  
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string current_string = "";
+        backtrack(n, 0, 0, current_string, result);
+        return result;
+    }
+};
+
+// Approach 2: BFS
+
+/*
+
+class Solution {
+public:
     struct State {
         string current_string;
         int open_count;
@@ -31,3 +62,5 @@ public:
         return result;
     }
 };
+
+*/
