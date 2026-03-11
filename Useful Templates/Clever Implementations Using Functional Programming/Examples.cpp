661889953b1361f1f81fac50025812c04d05e440
Asif Azam Biswas <asifazam2001@gmail.com>
2026-03-11 23:58:16 +0600

    Added examples demonstrating functional programming techniques in C++.

---
+++ Useful Templates/Clever Implementations Using Functional Programming/Examples.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+void solve() {
+    //std::any_of
+    //This is a very useful function that returns true if any of the elements in the range [first, last) satisfies some condition. Let's say you want to figure out if an array contains at least one 9. You could just write the naive loop and waste time in contest.
+
+    vector<int> a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    bool ok = 0;
+    for(int i = 0; i < (int)(a).size(); ++i) {
+        if(a[i] == 9) {
+            ok = 1;
+            break;
+        }
+    }
+
+    //but you could be smart and write this all in one line.
+    bool ok = any_of(a.begin(), a.end(), [](int x) { return x == 9; });
+}
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++) {
+        solve();
+    }
+
+    return 0;
+}
